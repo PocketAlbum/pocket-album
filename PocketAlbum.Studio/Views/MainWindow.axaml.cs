86a1de66304855a588c672c5da245ecec370c991
Jaka Kordez <jaka.kordez@gmail.com>
2025-11-29 07:45:19 +0100

    Added Avalonia project for PocketAlbum Studio

---
+++ PocketAlbum.Studio/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PocketAlbum.Studio.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
